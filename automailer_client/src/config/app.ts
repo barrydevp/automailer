dc2f0d3484ecb8b572bcc5880769279e66cecfee
barrydevp <barrydevp@gmail.com>
2023-11-03 04:22:58 +0000

    chore: update

--- automailer_client/src/config/app.ts
+++ automailer_client/src/config/app.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-  apiPrefix: "http://100.103.32.144:3000/api",
+  apiPrefix: import.meta.env.API_URL || "http://100.103.32.144:3000/api",
   authenticatedEntryPath: "/home",
   unAuthenticatedEntryPath: "/sign-in",
   tourPath: "/",
